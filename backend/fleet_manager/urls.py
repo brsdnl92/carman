789d71e06e4f56c9c47dde761ec4c6fa80587080
BJ <janos.b@freemail.hu>
2024-04-27 12:36:48 +0200

    navbar, uj oldal(onecar, statisztika) views, url, uj index oldal (kepeket berak)

--- backend/fleet_manager/urls.py
+++ backend/fleet_manager/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('contact/', views.contact),
     path('foglalas/', views.foglalas),
     path('premium/', views.premium),
+    path('onecar/', views.onecar),
+    path('statisztika/', views.statisztika),
     path('login/', views.login_page, name='login_page'),
     path('logout/', views.logout_page, name='logout_page'),
     path('register/', views.register_page, name='register_page'),
